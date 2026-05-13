0797aaab6bb930ce8328c713cdf71f552f04b16a
Devin Teichrow <devinteichrow@Mac.lan>
2026-05-13 00:24:25 -0700

    Automated Newsdesk refresh 2026-05-13 00:24 PDT

--- docs/atlases/pathogen/data/pathogen_atlas_data.js
+++ docs/atlases/pathogen/data/pathogen_atlas_data.js
@@ -882,7 +882,7 @@ window.PATHOGEN_ATLAS_DATA = {
       "related_stories": [
         {
           "display_title": "Hantavirus and cruise-ship outbreak",
-          "latest_update_summary": "New publisher/source coverage joined this story cluster: facebook.com.",
+          "latest_update_summary": "New publisher/source coverage joined this story cluster: Buenos Aires Times.",
           "story_id": "story_f6d225b01f3f7094",
           "story_url": "stories/story_f6d225b01f3f7094-hantavirus-and-cruise-ship-outbreak.html",
           "story_web_path": "stories/story_f6d225b01f3f7094-hantavirus-and-cruise-ship-outbreak.html",
@@ -998,7 +998,7 @@ window.PATHOGEN_ATLAS_DATA = {
           "related_stories": [
             {
               "display_title": "Hantavirus and cruise-ship outbreak",
-              "latest_update_summary": "New publisher/source coverage joined this story cluster: facebook.com.",
+              "latest_update_summary": "New publisher/source coverage joined this story cluster: Buenos Aires Times.",
               "story_id": "story_f6d225b01f3f7094",
               "story_url": "stories/story_f6d225b01f3f7094-hantavirus-and-cruise-ship-outbreak.html",
               "story_web_path": "stories/story_f6d225b01f3f7094-hantavirus-and-cruise-ship-outbreak.html",
@@ -1136,7 +1136,7 @@ window.PATHOGEN_ATLAS_DATA = {
           "related_stories": [
             {
               "display_title": "Hantavirus and cruise-ship outbreak",
-              "latest_update_summary": "New publisher/source coverage joined this story cluster: facebook.com.",
+              "latest_update_summary": "New publisher/source coverage joined this story cluster: Buenos Aires Times.",
               "story_id": "story_f6d225b01f3f7094",
               "story_url": "stories/story_f6d225b01f3f7094-hantavirus-and-cruise-ship-outbreak.html",
               "story_web_path": "stories/story_f6d225b01f3f7094-hantavirus-and-cruise-ship-outbreak.html",
@@ -1274,7 +1274,7 @@ window.PATHOGEN_ATLAS_DATA = {
           "related_stories": [
             {
               "display_title": "Hantavirus and cruise-ship outbreak",
-              "latest_update_summary": "New publisher/source coverage joined this story cluster: facebook.com.",
+              "latest_update_summary": "New publisher/source coverage joined this story cluster: Buenos Aires Times.",
               "story_id": "story_f6d225b01f3f7094",
               "story_url": "stories/story_f6d225b01f3f7094-hantavirus-and-cruise-ship-outbreak.html",
               "story_web_path": "stories/story_f6d225b01f3f7094-hantavirus-and-cruise-ship-outbreak.html",
@@ -4331,6 +4331,6 @@ window.PATHOGEN_ATLAS_DATA = {
       "reference_href": "../../reference/pertussis.html"
     }
   ],
-  "generated_at": "2026-05-12T23:21:46",
+  "generated_at": "2026-05-13T00:24:11",
   "atlas_count": 30
 };
